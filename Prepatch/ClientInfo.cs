d40f57f50faaffe10c57ff07f50630492cd69d6c
Matthew Christensen <christensenfamily05@gmail.com>
2025-10-28 15:24:00 -0500

    Changed prepatch GUID

--- Prepatch/ClientInfo.cs
+++ Prepatch/ClientInfo.cs
@@ -9,7 +9,7 @@ namespace MoreBotsAPI
     public static class ClientInfo
     {
         public const string GUID = "com.morebotsapi.tacticaltoaster";
-        public const string PreLoadGUID = "com.morebotsprepatch.tacticaltoaster";
+        public const string PreLoadGUID = "com.morebotsapi.tacticaltoaster";
         public const string PluginName = "MoreBotsAPI";
         public const string PreLoadName = "MoreBotsPrepatch";
         public const string Version = "1.0.0";
